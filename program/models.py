7236c7727161329d0ec36f0b6fc4b5d831395186
salah ahmed <salahs.email@pm.me>
2024-11-24 14:45:50 -0500

    formatting

--- program/models.py
+++ program/models.py
@@ -1,9 +1,22 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+
+class DaysPerWeek(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
 
 
 # Create your models here.
 class Program(models.Model):
     name = models.CharField(max_length=200)
-    weeks = models.PositiveSmallIntegerField()
+    weeks = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(4), MaxValueValidator(16)]
+    )
     description = models.TextField()
-    days_per_week = models.PositiveSmallIntegerField(choices=range(1, 8))
+    days_per_week = models.IntegerField(choices=DaysPerWeek)
