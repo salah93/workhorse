125a13a1cb264be48a873b1439720f32ccdaa0a9
salah ahmed <salahs.email@pm.me>
2024-11-24 18:25:25 -0500

    add inline for days

--- program/admin.py
+++ program/admin.py
@@ -3,10 +3,26 @@ from django.contrib import admin
 from .models import Day, Exercise, Info
 
 
+@admin.action(description="Create Templates for Program")
+def make_strongapp_templates_for_program(modeladmin, request, queryset):
+    pass
+
+
+@admin.action(description="Create Template for Program Day")
+def make_strongapp_template_for_day(modeladmin, request, queryset):
+    pass
+
+
+class DayAdminInline(admin.TabularInline):
+    model = Day
+
+
 @admin.register(Info)
 class InfoAdmin(admin.ModelAdmin):
     fields = ["name", ("weeks", "days_per_week"), "description"]
     list_display = ["name"]
+    actions = [make_strongapp_templates_for_program]
+    inlines = [DayAdminInline]
 
 
 class ExerciseInline(admin.TabularInline):
@@ -16,3 +32,4 @@ class ExerciseInline(admin.TabularInline):
 @admin.register(Day)
 class DayAdmin(admin.ModelAdmin):
     inlines = [ExerciseInline]
+    actions = [make_strongapp_template_for_day]
