7772078fc0db0726bbea3519fe9de34344b198b4
salah ahmed <salahs.email@pm.me>
2024-11-30 17:03:49 -0500

    add column hevy-template-id

--- exercise/models.py
+++ exercise/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     name = models.CharField(
         max_length=50, unique=True, verbose_name="Category"
     )
+    hevy_template_id = models.CharField(max_length=10, unique=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -14,6 +15,7 @@ class BodyPart(models.Model):
     name = models.CharField(
         max_length=50, unique=True, verbose_name="Body Part"
     )
+    hevy_template_id = models.CharField(max_length=10, unique=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -24,6 +26,7 @@ class Info(models.Model):
     body_part = models.ForeignKey(BodyPart, on_delete=models.PROTECT)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     image = models.ImageField(upload_to="exercises", null=True, blank=True)
+    hevy_template_id = models.CharField(max_length=10, unique=True, null=True)
 
     class Meta:
         verbose_name = "Exercise"
