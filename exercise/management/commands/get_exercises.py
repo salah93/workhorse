8ed81874c1c655b6f36802754b5abddc742bea5f
salah ahmed <salahs.email@pm.me>
2024-11-30 17:23:16 -0500

    get all exercises

--- exercise/management/commands/get_exercises.py
+++ exercise/management/commands/get_exercises.py
@@ -12,38 +12,42 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         limit = 100
         page = 1
-        response = requests.get(
+        while response := requests.get(
             f"https://api.hevyapp.com/v1/exercise_templates?page={page}&pageSize={limit}",
             headers={
                 "accept": "application/json",
                 "api-key": settings.HEVY_API_KEY,
             },
-        )
-        if response.status_code != 200:
-            raise CommandError("got bad response %s" % response.status_code)
+        ):
+            if response.status_code != 200:
+                raise CommandError(
+                    "got bad response %s" % response.status_code
+                )
+            page += 1
 
-        for exercise in response.json()["exercise_templates"]:
-            self.stdout.write(
-                self.style.NOTICE("writing exercise %s" % exercise["title"])
-            )
+            for exercise in response.json()["exercise_templates"]:
+                self.stdout.write(
+                    self.style.NOTICE(
+                        "writing exercise %s" % exercise["title"]
+                    )
+                )
 
-            category, _ = Category.objects.get_or_create(
-                hevy_template_id=exercise["id"],
-                defaults={"name": exercise["equipment"]},
-            )
-            body_part, _ = BodyPart.objects.get_or_create(
-                hevy_template_id=exercise["id"],
-                defaults={"name": exercise["primary_muscle_group"]},
-            )
+                category, _ = Category.objects.get_or_create(
+                    name=exercise["equipment"]
+                )
+                body_part, _ = BodyPart.objects.get_or_create(
+                    name=exercise["primary_muscle_group"]
+                )
 
-            Exercise.objects.get_or_create(
-                hevy_template_id=exercise["id"],
-                defaults={
-                    "name": exercise["title"],
-                    "category": category,
-                    "body_part": body_part,
-                },
-            )
+                try:
+                    Exercise.objects.get(name=exercise["title"])
+                except Exercise.DoesNotExist:
+                    Exercise.objects.create(
+                        hevy_template_id=exercise["id"],
+                        name=exercise["title"],
+                        category=category,
+                        body_part=body_part,
+                    )
 
         self.stdout.write(
             self.style.SUCCESS("Successfully copied all exercises")
