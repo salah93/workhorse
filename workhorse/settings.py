987f8257857a038624b8a2730c416dbe59ca338a
salah ahmed <salahs.email@pm.me>
2024-11-30 17:04:56 -0500

    add get-exercises command

--- workhorse/settings.py
+++ workhorse/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+HEVY_API_KEY = os.environ["HEVY_API_KEY"]
