3e2cecfc74a3c5486f3ff97336197794aacbe378
salah ahmed <salahs.email@pm.me>
2024-11-30 19:43:40 -0500

    update change permission

--- user/admin.py
+++ user/admin.py
@@ -6,3 +6,8 @@ from user.models import Profile1RM
 @admin.register(Profile1RM)
 class ProfileAdmin(admin.ModelAdmin):
     search_fields = ["user__username"]
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return obj is None or obj.created_by == request.user
